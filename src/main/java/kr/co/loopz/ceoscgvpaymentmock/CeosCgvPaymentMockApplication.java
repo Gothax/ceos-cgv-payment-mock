bb4ba8bf03e19fd30184bf020e897fc2c9ea1a8e
Gothax <scott990328@gmail.com>
2025-09-22 17:40:56 +0900

    feat: init project

---
+++ src/main/java/kr/co/loopz/ceoscgvpaymentmock/CeosCgvPaymentMockApplication.java
@@ -0,0 +1,13 @@
+package kr.co.loopz.ceoscgvpaymentmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeosCgvPaymentMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeosCgvPaymentMockApplication.class, args);
+    }
+
+}
